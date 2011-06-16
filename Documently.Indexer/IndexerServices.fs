5c15e5ff918ab9e175b902416d6ebde5c3a1e3cb
Henrik <henrik@haf.se>
2011-06-16 21:20:40 +0200

    starting to integrate the f# code now, but the tests, besides that for the indexer are passing

---
+++ Documently.Indexer/IndexerServices.fs
@@ -0,0 +1,8 @@
+﻿namespace Documently.Indexer
+  open MassTransit
+  open Documently.Domain.Events
+  
+  type IndexerService(busIn:IServiceBus) =
+    let bus = busIn
+    interface Consumes<AssociatedIndexingPending>.All with
+      member x.Consume msg = printf "msg.%A" msg.BlobId

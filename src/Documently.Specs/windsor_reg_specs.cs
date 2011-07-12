da00f70a780503bc2dc31b96150ca996d846780d
Henrik <henrik@haf.se>
2011-07-12 14:19:38 +0200

    all tests passing

--- src/Documently.Specs/windsor_reg_specs.cs
+++ src/Documently.Specs/windsor_reg_specs.cs
@@ -4,7 +4,10 @@ using Castle.Windsor;
 using Documently.Domain.Events;
 using Documently.Infrastructure;
 using Documently.ReadModel;
+using MassTransit;
+using Moq;
 using NUnit.Framework;
+using Raven.Client;
 using SharpTestsEx;
 
 namespace CQRSSample.Specs
@@ -15,10 +18,16 @@ namespace CQRSSample.Specs
 		public void registers_all_views()
 		{
 			var c = new WindsorContainer();
-			c.Register(Component.For<IBus>().ImplementedBy<InProcessBus>());
-			RegisterEventHandlersInBus.BootStrap(c);
 
-			c.ResolveAll<HandlesEvent<CustomerCreatedEvent>>()
+			c.Register(
+				Component.For<IDocumentStore>().Instance(new Mock<IDocumentStore>().Object),
+				Component.For<IWindsorContainer>().Instance(c),
+				Component.For<IBus>().ImplementedBy<InProcessBus>(),
+				AllTypes.FromAssembly(typeof(CustomerListView).Assembly)
+					.BasedOn(typeof(Consumes<>.All))
+					.WithService.FromInterface(typeof(Consumes<>.All)));
+
+			c.ResolveAll<Consumes<CustomerCreatedEvent>.All>()
 				.Any(x => x.GetType().Equals(typeof(CustomerAddressView)))
 				.Should(" the address view should be in the enumerable of those handlers").Be.True();
 		} 

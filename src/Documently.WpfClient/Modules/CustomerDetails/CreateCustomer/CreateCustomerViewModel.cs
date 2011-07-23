d1b80a7a856d33c934d349ca75e8f812559435e7
Henrik <henrik@haf.se>
2011-07-23 03:01:44 +0200

    almost fixed wpf client

--- src/Documently.WpfClient/Modules/CustomerDetails/CreateCustomer/CreateCustomerViewModel.cs
+++ src/Documently.WpfClient/Modules/CustomerDetails/CreateCustomer/CreateCustomerViewModel.cs
@@ -2,15 +2,16 @@ using System;
 using Caliburn.Micro;
 using Documently.Commands;
 using Documently.Infrastructure;
+using MassTransit;
 
 namespace Documently.WpfClient.Modules.CustomerDetails.CreateCustomer
 {
 	public class CreateCustomerViewModel : Screen
 	{
-		private readonly IBus _Bus;
+		private readonly IServiceBus _Bus;
 		private readonly IEventAggregator _EventAggregator;
 
-		public CreateCustomerViewModel(IBus bus, IEventAggregator eventAggregator)
+		public CreateCustomerViewModel(IServiceBus bus, IEventAggregator eventAggregator)
 		{
 			_Bus = bus;
 			_EventAggregator = eventAggregator;
@@ -22,7 +23,7 @@ namespace Documently.WpfClient.Modules.CustomerDetails.CreateCustomer
 		public void Save()
 		{
 			//important: send command over bus
-			_Bus.Send(Command);
+			_Bus.Publish(Command);
 
 			//signal for UI - change view
 			_EventAggregator.Publish(new CreateCustomerSavedEvent());

73d32fae26c8cc4ef128b1fe99382240799f8184
whyer <addeman@gmail.com>
2011-11-26 15:54:26 +0100

    Crated DocumentCollection aggregate

---
+++ src/Documently.Domain/Domain/DocumentCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CommonDomain.Core;
+using Documently.Domain.Events;
+
+namespace Documently.Domain.Domain
+{
+    public class DocumentCollection : AggregateBase
+    {
+        public DocumentCollection()
+        {
+            var @event = new DocumentCollectionCreated(Id, "Default name");
+            RaiseEvent(@event);
+        }
+
+        public void Apply(DocumentCollectionCreated evt)
+        {
+            this.Id = evt.AggregateId;
+        }
+
+        
+    }
+
+    public class DocumentCollectionCreated : DomainEvent
+    {
+        public string Name { get; protected set; }
+
+        public DocumentCollectionCreated(Guid id, string name)
+        {
+            Name = name;
+            AggregateId = id;
+        }
+    }
+}

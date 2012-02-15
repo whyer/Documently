d84fec5f58b805c8f37f1b6ae3c87b52c7570585
Henrik <henrik@haf.se>
2012-02-15 21:06:23 +0100

    newer assemblies for the listener which now is in the clients sln folder

---
+++ src/Documently.SampleEventListener/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Documently.Domain.Events;
+using Magnum.Extensions;
+using MassTransit;
+
+namespace Documently.SampleEventListener
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var bus = ServiceBusFactory.New(cfg =>
+				{
+					cfg.ReceiveFrom("rabbitmq://localhost/Documently.SampleEventListener");
+					cfg.UseRabbitMqRouting();
+					cfg.Subscribe(s =>
+						s.Handler<CustomerCreatedEvent>(
+							created => Console.WriteLine("Customer Created: {0}".FormatWith(created)))
+							.Transient());
+				});
+
+			Console.ReadKey(true);
+
+			bus.Dispose();
+		}
+	}
+}
